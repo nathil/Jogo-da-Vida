9c198311234debd0066c5cf1d4eacc01ff3d302c
nathil <nathiellycastro282@gmail.com>
2025-08-22 19:28:41 -0300

    função finalizada

--- automato.js
+++ automato.js
@@ -1,26 +1,31 @@
 
-function retorna_valor(cadeia){
-    var automato = [
-        [1,4],
-        [1,2],
-        [2,3],
-        [3,6],
-        [4,5],
-        [5,7],
-        [6,8],
-        [7,6],
-        [8,8]      
-    ]; 
-    
-    var estado, novo_estado; 
-    
-    for(var i=0; i<9; i++){
-        simbolo = cadeia[i]; 
-        novo_estado = automato[estado][simbolo];  
+const automato = [
+    [1,4],
+    [1,2],
+    [2,3],
+    [3,6],
+    [4,5],
+    [5,7],
+    [6,8],
+    [7,6],
+    [8,8]      
+]; 
 
-        if(estado == 6 || estado == 7){
-            return true; 
-        }
-        return false; 
+/** 
+ * Válida uma cadeia que é representada por uma célula e seus 8 vizinhos.
+ * 
+ * @param {string} cadeia - Representa o estado atual da célula (y) e seus 8 vizinhos (x) no formato yxxxxxxxx.
+ * @returns {boolean} - Representa o novo estado da célula, se está viva ou morta, e também se a cadeia é aceita. 
+*/
+function validaCadeia(cadeia){    
+
+    let estado = 0, novo_estado = 0; 
+    
+    for(let i=0; i<9; i++){ 
+        simbolo = cadeia[i]; 
+        novo_estado = novo_estado + automato[estado][simbolo];  
+        estado = novo_estado;  
     }
+
+    return estado === 6 || estado === 7; 
 }
